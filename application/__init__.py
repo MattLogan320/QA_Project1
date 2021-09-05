009a6f442a2d52ebc1ccd1874e2862d3e7c2d870
MattLogan320 <matt.logan320@gmail.com>
2021-09-05 15:29:30 +0100

    Initial requirements added, gitignore created with some things to be ignored already stated. Basic code for create and app written

---
+++ application/__init__.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
+
+
+from application import routes

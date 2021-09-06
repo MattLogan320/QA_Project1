d29061863d1256d8bfb452b95908dea203c0e8b2
MattLogan320 <matt.logan320@gmail.com>
2021-09-06 21:52:51 +0100

    Routes finished off and some html templates started. After running test of app, os had to be removed from requirements.txt but still seems to run correctly

--- application/models.py
+++ application/models.py
@@ -6,16 +6,16 @@ class Books(db.Model):
     bookName = db.Column(db.String(150), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     book_loaned = db.Column(db.String(5), nullable=False)
-    customerBook1= db.relationship('CustomerBoooks', backref=db.backref('book',lazy = True))
+    customerBook1= db.relationship('CustomerBooks', backref=db.backref('book',lazy = True))
 
 
 class Customers(db.Model):
     id =db.Column(db.Integer, primary_key=True)
     customerName = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
-    customerBook2= db.relationship('CustomerBoooks', backref=db.backref('customer',lazy = True))
+    customerBook2= db.relationship('CustomerBooks', backref=db.backref('customer',lazy = True))
 
 class CustomerBooks(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     fk_book_id=db.Column(db.Integer,db.ForeignKey('books.id'))
-    fk_customer_id = db.Column(db.Integer,db.ForeignKey('customer.id'))
+    fk_customer_id = db.Column(db.Integer,db.ForeignKey('customers.id'))
--- application/routes.py
+++ application/routes.py
@@ -8,11 +8,8 @@ from application.forms import BookForm, CustomerForm, UpdateBookForm, UpdateCust
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/library')
-def library():
-    bookList= Books.query.all()
-    return render_template('library.html', title='Library', books=bookList)
 
+#Create Functions
 @app.route('/addbook', methods =['GET','POST'])
 def addbook():
     form=BookForm()
@@ -29,17 +26,12 @@ def addbook():
         print('Please fill out all boxes')
     return render_template('addbook.html', title= 'Add Book', form=form)
 
-@app.route('/customerlist')
-def customerlist():
-    customerList= Customers.query.all()
-    return render_template('customerlist.html', title='Customer List', customers=customerList)
-
 @app.route('/customer', methods= ['GET', 'POST'])
 def addcustomer():
     form=CustomerForm()
     if form.validate_on_submit():
         customerList=Customers(
-          customerNam=form.customerName.data,
+          customerName=form.customerName.data,
           surname=form.surname.data  
         )
         db.session.add(customerList)
@@ -50,7 +42,7 @@ def addcustomer():
     return render_template('customers.html', title='Add customer', form =form)
 
 @app.route('/addbookcustomer', methods=['GET', 'POST'])
-def querysetsong():
+def borrowbook():
 	bookList = Books.query.all()
 	customerList = Customers.query.all()
 	form = CustomerBookForm()
@@ -62,3 +54,72 @@ def querysetsong():
 		db.session.add(customerBook)
 		db.session.commit()
 	return render_template('addbookcustomer.html', title='Borrow book', books=bookList, customers=customerList, form=form)
+
+#Read Functions
+@app.route('/library')
+def library():
+    bookList= Books.query.all()
+    return render_template('library.html', title='Library', books=bookList)
+
+@app.route('/customerlist')
+def customerlist():
+    customerList= Customers.query.all()
+    return render_template('customerlist.html', title='Customer List', customers=customerList)
+
+@app.route('/read/<id>', methods= ['GET'])
+def read_customers_borrowed(id):
+    bookList=Books.query.join(CustomerBooks).filter_by(fk_customer_id=id).all()
+    return render_template('read.html',title='Borrowed books', books=bookList)
+
+#Update Functions
+@app.route('/library/edit/<id>',methods=['GET','POST'])
+def book_edit(id):
+    book= Books.query.filter_by(id=id).first()
+    form= UpdateBookForm()
+    if form.validate_on_submit:
+        book.bookName=form.bookName.data,
+        book.author=form.author.data,
+        book.book_loaned=form.book_loaned.data
+        db.session.commit()
+        return redirect(url_for('library'))
+    elif request.method == 'GET':
+        form.bookName.data= book.bookName
+        form.author.data= book.author
+        form.book_loaned.data= book.book_loaned
+    return render_template('editbook.html', title= 'Edit Book Details', form=form)
+
+@app.route('/customerlist/edit/<id>', methods=['GET','POST'])
+def edit_customer():
+    customer=Customers.query.filter_by(id=id).first()
+    form=UpdateCustomerForm()
+    if form.validate_on_submit():
+        customer.customerName=form.customerName.data
+        customer.surname=form.surname.data
+        db.session.commit()
+        return redirect(url_for('customerlist'))
+    elif request.method== 'GET':
+        form.customerName.data = customer.customerName
+        form.surname.data= customer.surname
+    return render_template('editcustomer', title='Edit customer details', form=form)
+
+#Delete Functions
+@app.route('/library/delete/<id>', methods=['GET','POST'])
+def book_delete():
+    customerBook=CustomerBooks.query.join(Books).filter_by(id=id).all()
+    for book in customerBook:
+        db.session.delete(book)
+    book= Books.query.filter_by(id=id).first()
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('library'))
+
+@app.route('/customerlist/delete/<id>', methods=['GET','POST'])
+def customer_delete(id):
+    customerBook=CustomerBooks.query.join(Customers).filter_by(id=id).all()
+    for customer in customerBook:
+        db.session.delete(customer)
+    customer=Customers.query.filter_by(id=id).first()
+    db.session.delete(customer)
+    db.session.commit()
+    return redirect(url_for('customerlist'))
+
